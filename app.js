c55d00a6b542e22f6d48be3e63af3bfbe26db958
andrewkimnz <andrewsiwookim@gmail.com>
2020-05-29 22:05:47 +1200

    Commit #2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const ScoreDisplay = document.querySelector('#score')
   const StartBtn = document.querySelector('#start-button')
   const width = 10
-
+  //
   // The Tetrominoes
   const lTetromino = [
     [1, width+1, width*2+1, 2],
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
     [1, width+1, width*2+1, width*2],
     [width, width*2, width*2+1, width*2+2]
   ]
-//
+
   const zTetromino = [
     [0,width,width+1,width*2+1],
     [width+1, width+2,width*2,width*2+1],
